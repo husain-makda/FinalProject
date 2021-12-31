fd7a9ba04c5236564fbbace4737cda12d47b2923
Husain Makda <husain.makda@nfocus.co.uk>
2021-12-31 15:53:41 +0000

    added selenium test

---
+++ FinalProject.Husain/Utils/BaseClass.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace FinalProject.Husain.Utils
+{
+    public class BaseClass
+    {
+        public IWebDriver driver;
+        public string baseUrl = "https://www.edgewordstraining.co.uk/demo-site";
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://www.edgewordstraining.co.uk/demo-site";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            //driver.Quit();
+        }
+    }
+}
---
+++ FinalProject.Husain/Utils/MyHelpers.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Husain.Utils
+{
+    public static class MyHelpers
+    {
+        public static void WaitHelper(IWebDriver driver, int Seconds, By locator)
+        {
+            WebDriverWait mysecodwait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            mysecodwait.Until(drv => drv.FindElement(locator).Displayed);
+
+        }
+
+        public static void HandleAlert(IWebDriver driver)
+        {
+            IAlert myalert = driver.SwitchTo().Alert();
+            Console.WriteLine("Alert encountered wit text : " + myalert.Text);
+            myalert.Accept();
+        }
+
+        public static void TakeScreenshot(IWebDriver driver, string Filename)
+        {
+            
+            ITakesScreenshot ssdriver = driver as ITakesScreenshot;
+            Screenshot myscreenshot =  ssdriver.GetScreenshot();
+            myscreenshot.SaveAsFile(@"C:\Users\HusainMakda\OneDrive - nFocus Limited\Pictures\Screenshots\" + Filename + ".png", ScreenshotImageFormat.Png);
+
+        }
+
+        public static void TakeScreenshotElement(IWebDriver driver, string Filename, By locator)
+        {
+            IWebElement myelm = driver.FindElement(locator);
+            ITakesScreenshot ssdriver = myelm as ITakesScreenshot;
+            Screenshot myscreenshot = ssdriver.GetScreenshot();
+            myscreenshot.SaveAsFile(@"C:\Users\HusainMakda\OneDrive - nFocus Limited\Pictures\Screenshots\" + Filename + ".png", ScreenshotImageFormat.Png);
+
+        }
+    }
+}
